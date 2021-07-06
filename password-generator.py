7ca732a133a69fbdc6a298d80756348612bbfa39
infinitecoder1729 <77016507+infinitecoder1729@users.noreply.github.com>
2021-07-07 00:28:16 +0530

    Create password-generator.py

---
+++ password-generator.py
@@ -0,0 +1,67 @@
+capchar=['A','B','C','D','E','F','G','H','I','J','K','L','M','N','O','P','Q','R','S','T','U','V','W','X','Y','Z']
+dig = ['0', '1', '2', '3', '4', '5', '6', '7', '8', '9']
+spchar=['!','@','#','$','%','^','&','*','(',')','_','-',',','.']
+smallchar=['a', 'b', 'c', 'd', 'e', 'f', 'g', 'h','i', 'j', 'k', 'm', 'n', 'o', 'p', 'q','r', 's', 't', 'u', 'v', 'w', 'x', 'y','z'] 
+import random
+passlen=int(input("Enter the desired length of password : "))
+capans=str(input("Are CAPITAL Alphabets allowed ? (Y/N) : "))
+smallans=str(input("Are lowercase Alphabets allowed ? (Y/N) : "))
+digans=str(input("Are Digits allowed ? (Y/N) : "))
+spans=str(input("Are Special Charecters allowed ? (Y/N) : "))
+capans,smallans,digans,spans=capans.title(),smallans.title(),digans.title(),spans.title()
+if capans=='Y':
+     if smallans=='Y':
+          if digans=='Y':
+               if spans=='Y':
+                    comblis=capchar+dig+spchar+smallchar
+               else:
+                    comblis=capchar+dig+smallchar
+          else :
+               if spans=='Y':
+                    comblis=capchar+spchar+smallchar
+               else:
+                    comblis=capchar+smallchar
+     else:
+          if digans=='Y':
+               if spans=='Y':
+                    comblis=capchar+dig+spchar
+               else:
+                    comblis=capchar+dig
+          else :
+               if spans=='Y':
+                    comblis=capchar+spchar
+               else:
+                    comblis=capchar
+else :
+     if smallans=='Y':
+          if digans=='Y':
+               if spans=='Y':
+                    comblis=dig+spchar+smallchar
+               else:
+                    comblis=dig+smallchar
+          else :
+               if spans=='Y':
+                    comblis=spchar+smallchar
+               else:
+                    comblis=smallchar
+     else:
+          if digans=='Y':
+               if spans=='Y':
+                    comblis=dig+spchar
+               else:
+                    comblis=dig
+          else :
+               if spans=='Y':
+                    comblis=ar+spchar
+               else:
+                    print("Error, You denied the usage of all possible charecters")
+                    quit()
+
+password=""
+random.shuffle(comblis)
+for i in range(passlen):
+     q=random.choice(comblis)
+     random.shuffle(comblis)
+     password=password+q
+print("\nGenerated Password is : ",password)
+print()
